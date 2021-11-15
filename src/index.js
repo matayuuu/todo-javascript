96506f511dac3a42ae5272797f01d7703625754a
matayuuu <65808410+matayuuu@users.noreply.github.com>
2021-11-16 00:28:57 +0900

    1115

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const addKlick = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //divタグの生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  //liタグの生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+  //console.log(li);
+
+  //divタグの子要素に各要素を設定
+  div.appendChild(li);
+  //console.log(div);
+
+  //未完了リストにdivタグを追加
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => addKlick());
