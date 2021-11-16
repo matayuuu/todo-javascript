14e8792763ca7b5db36e08ffb0e79641fb4f9178
matayuuu <65808410+matayuuu@users.noreply.github.com>
2021-11-16 09:58:07 +0900

    完了・削除ボタン作成

--- src/index.js
+++ src/index.js
@@ -8,13 +8,28 @@ const addKlick = () => {
   const div = document.createElement("div");
   div.className = "list-row";
 
-  //liタグの生成
+  //liタグの生成　　//liタグにテキストを入力
   const li = document.createElement("li");
   li.innerText = inputText;
-  //console.log(li);
+
+  //Button（完了）の作成
+  const finishButton = document.createElement("button");
+  finishButton.innerText = "完了";
+  finishButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  //button(削除)の作成
+  const deliteButton = document.createElement("button");
+  deliteButton.innerText = "削除";
+  deliteButton.addEventListener("click", () => {
+    alert("削除");
+  });
 
   //divタグの子要素に各要素を設定
   div.appendChild(li);
+  div.appendChild(finishButton);
+  div.appendChild(deliteButton);
   //console.log(div);
 
   //未完了リストにdivタグを追加
@@ -24,3 +39,6 @@ const addKlick = () => {
 document
   .getElementById("add-button")
   .addEventListener("click", () => addKlick());
+
+//完了リストにdivタグを追加
+//document.getElementById("comlete-area").appendChild(div);
